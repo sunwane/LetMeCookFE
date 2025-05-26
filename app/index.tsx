087062b31bedefb7ee472dc89e4a73cb19ccd386
sunwane <auxuanhoa@gmail.com>
2025-05-26 10:10:39 +0700

    init

---
+++ app/index.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>What the hell, con chuột của tôi đâu?</Text>
+    </View>
+  );
+}
