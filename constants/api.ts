7c5239caf92e37e13060d27a3503ddd4c656dae9
hiuth <hiuth2004@gmail.com>
2025-06-21 17:24:02 +0700

    my branch

--- constants/api.ts
+++ constants/api.ts
@@ -1,4 +1,4 @@
-export const API_BASE_URL = "http://192.168.1.5:8080";
+export const API_BASE_URL = "http://192.168.1.14:8080";
 
 // ✅ Instructions for other developers:
 // Change the IP address above to your local machine IP
